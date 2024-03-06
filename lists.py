908a1aaaed723b5357a76140199abfddcfdc3e0a
nzommmo <ericnzomo17@gmail.com>
2024-03-06 23:50:42 +0300

    Week 2 assignment

---
+++ lists.py
@@ -0,0 +1,15 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1,15)
+my_list2 = [50,60,70]
+my_list.extend(my_list2)
+my_list.sort()
+index0f30 = my_list.index(30)
+print(index0f30)
+
+
+
+print(my_list)
