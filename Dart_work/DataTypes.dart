b62882b7fa1199e6ccb08585387b680cbddcb0be
nzommmo <ericnzomo17@gmail.com>
2024-03-07 00:16:38 +0300

    Dart week 2 assignment

--- Dart_work/DataTypes.dart
+++ Dart_work/DataTypes.dart
@@ -2,8 +2,8 @@ void main(){
     // Declaring Variables
     String name = "John";
     String address = "Kenya";
-    num age = 20;
-    num height = 5.9;
+    int age = 20;
+    double height = 5.9;
     bool isMarried = false;
 
     //Printing Variables
@@ -11,30 +11,31 @@ void main(){
     print("Address is $address");
     print("Age is $age");
     print("Height is $height");
-    print("Married Status is $isMarried");
+    print("Married Status is $isMarried\n");
 
 
-
+    //Creating a String
     List<String> Names = ["Eric","Nzomo","Jorja","Brent"];
+    //Printing a String
     print("Value of names is $Names");
     print("Value of names[0] is ${Names[0]}");
     print("Value of names[1] is ${Names[1]}");
-    print("Value of names[2] is ${Names[2]}");
+    print("Value of names[2] is ${Names[2]}\n");
 
     print(Names);
 
 
 
-
+    //Creating a Map
     Map<String,int> ages = {
         'Eric':20,
         'John':25,
         'Charles':30,
         'Fiona':29,
         };
+    //Printing Map Values
     print("Ages of students: $ages");
 
 
-    String runeString = "Runes in Dart:\u{1F600} \u{1F64B}";
-    print(runeString);
+    
 }
