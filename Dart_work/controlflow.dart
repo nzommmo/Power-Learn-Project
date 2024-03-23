e19370ff844697e38558e854a80bae0225133f44
nzommmo <ericnzomo17@gmail.com>
2024-03-23 22:51:57 +0300

    dart assignment

---
+++ Dart_work/controlflow.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+
+  stdout.writeln("Input your number");
+  int ? number = int.parse(stdin.readLineSync()!);
+
+  
+  if( number > 10){
+    print("Your number is greater than 10 ");
+  } else if (number < 10){
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+  } 
